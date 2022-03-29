30aca44fb97803144ed85c8b81a207fbd84f72e5
fire1ce <stas@3os.org>
2022-03-29 16:57:39 +0300

    updated startup script

--- 99-failover-telegram-notifications.sh
+++ 99-failover-telegram-notifications.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Creat a cronjob to start the script
-touch /etc/cron.d/failover-telegram-notifications
-echo "@reboot /bin/bash /mnt/data/UDMP-Failover-Telegram-Notifications/failover-notifications.sh" >/etc/cron.d/failover-telegram-notifications
-
-# Reaload the cron daemon to start the script once in background
-/etc/init.d/crond reload /etc/cron.d/failover-telegram-notifications
-/etc/init.d/crond restart
-
-exit 1
+# Start the Script at boot
+sleep 10
+/bin/bash /mnt/data/UDMP-Failover-Telegram-Notifications/failover-notifications.sh
+exit 0
